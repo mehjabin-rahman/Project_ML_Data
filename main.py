64535f51dc7b1800436fe854136ea0533e206039
mehjabin-rahman <mehjabin.rahman.ame@gmail.com>
2025-02-04 11:40:32 -0500

    validation stage added

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME="Data Integration stage"
 try:
         logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<\n\nx========x")
         obj=DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME="Data Validation stage"
+try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<\n\nx========x")
+        obj=DataValidationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx========x")
 except Exception as e:
